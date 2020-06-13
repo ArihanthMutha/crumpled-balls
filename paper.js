c63c65f5186f03319525dc9add53ababb5cdefcb
ArihanthMutha <62700226+ArihanthMutha@users.noreply.github.com>
2020-06-13 20:32:03 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,21 @@
+class Paper{
+    constructor(x,y){
+        var options={
+            isStatic:false,
+            friction:0.5,
+            restitution:0.3,
+            density:1.2
+
+        }
+        this.body=Bodies.circle(x,y,10,options)
+        this.radius=10;
+        World.add(world,this.body)
+    }
+    display(){
+        
+        ellipseMode(RADIUS)
+        fill("pink")
+        ellipse(this.body.position.x,this.body.position.y,10,10)
+       
+    }
+}
